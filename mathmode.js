fa293112f874dc34523364b88d85dcf7bdafc4c4
Ian Macalinao <me@ian.pw>
2015-01-05 00:28:01 -0600

    Format

--- mathmode.js
+++ mathmode.js
@@ -1,32 +1,32 @@
-var latex   = require("latex");
-var spawn   = require("child_process").spawn;
+var latex = require("latex");
+var spawn = require("child_process").spawn;
 
 function sanitize(expr) {
   var sanitized = expr.replace(/([^\\](\\\\)*)(\$)/g, "$1");
-  if(sanitized.charAt(0) === "$") {
+  if (sanitized.charAt(0) === "$") {
     return sanitized.substr(1)
   }
   return sanitized
 }
 
 module.exports = function(expr, options) {
-  if(!options) {
+  if (!options) {
     options = {};
   }
   var format = options.format || "png";
-  var size   = options.dpi    || 300;
-  
-  var package_list = options.packages || [ "amsmath" ];
+  var size = options.dpi || 300;
+
+  var package_list = options.packages || ["amsmath"];
   var packages = [];
-  for(var i=0; i<package_list.length; ++i) {
+  for (var i = 0; i < package_list.length; ++i) {
     var pkg = package_list[i];
-    if(pkg instanceof Array) {
+    if (pkg instanceof Array) {
       packages.push("\\usepackage[" + pkg[1] + "]{" + pkg[0] + "}");
     } else {
       packages.push("\\usepackage{" + pkg + "}");
     }
   }
-  
+
   var tex_stream = latex([
     "\\nonstopmode",
     "\\documentclass{minimal}",
@@ -40,10 +40,10 @@ module.exports = function(expr, options) {
     "$ \\end{preview}",
     "\\end{document}"
   ], {
-    command:  options.pdflatex_path || "pdflatex",
-    format:   "pdf"
+    command: options.pdflatex_path || "pdflatex",
+    format: "pdf"
   });
-  
+
   var convert_path = options.imagemagick_path || "convert";
   var convert = spawn(convert_path, [
     "-trim",
@@ -52,7 +52,7 @@ module.exports = function(expr, options) {
     "pdf:-",
     format + ":-"
   ]);
-  
+
   var result = convert.stdout;
   tex_stream.on("error", function(err) {
     result.emit("error", err);
